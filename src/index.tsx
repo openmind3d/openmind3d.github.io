ea4935683fbca602e2ad2831990f9c2d9c27feef
Alexander Pirogov <pirogov27@yahoo.com>
2023-01-08 15:07:45 +0300

    wip

--- src/index.tsx
+++ src/index.tsx
@@ -4,17 +4,5 @@ import { render } from 'solid-js/web';
 import './index.css';
 import App from './App';
 
-function Index() {
-    return (
-        <html>
-            <head>
 
-            </head>
-            <body>
-                Alexander Pirogov
-            </body>
-        </html>
-    )
-}
-
-render(() => <Index />, document.getElementById('root') as HTMLElement);
+render(() => <App />, document.getElementById('root') as HTMLElement);
